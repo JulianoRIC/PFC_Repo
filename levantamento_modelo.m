7a6859e4cbf1853f5da78086fb37bff83c8bcaa1
Juliano R.S. <jrs96.jr@gmail.com>
2023-09-12 15:19:26 -0300

    modelo levantado e projeto control

--- levantamento_modelo.m
+++ levantamento_modelo.m
@@ -18,9 +18,9 @@ figWidth = monWidth/3;
 %% Extracao dos dados experimentais
 
 nCol = 4; % numero colunas
-nSam = 152945; % numero de amostras 
+nSam = 402016; % numero de amostras 
 
-fName = '20230829_173404_heater___b_semconvec.txt';
+fName = '20230904_171958_heater___b_comconvec2.txt';
 fileID = fopen(fName,'r');
 formatSpec = repmat('%f',1,nCol);
 A = fscanf(fileID,formatSpec,[nCol nSam]);
@@ -32,6 +32,8 @@ vecT4   = A(:,2); %temperatura CH4
 vecT9   = A(:,3); %temperatura CH9
 vecPWM  = A(:,4); %duty cycle PWM
 
+%media temperaturas
+vecTM = (vecT4 + vecT9)/2;
 
 %% Graficos dos dados experimentais
 
@@ -42,98 +44,193 @@ set(gcf,'name','Temperaturas')
 grid on
 hold on
 
-%primeiro valor de temperatura acima de zero
-first =  find(vecT4>0, 1);
+first =  find(vecT4>0, 1); %primeiro valor de temperatura acima de zero
 plot(vecIter(first:end),vecT4(first:end))
 plot(vecIter(first:end),vecT9(first:end))
+plot(vecIter(first:end),vecTM(first:end))
 %plot(vecIter(first:end),vecPWM(first:end))
+legend('CH4','CH9','TM');
 
+%% selecionando a partir do valor degrau 11% --> 13% duty cycle
+
+figure
+subplot(2,1,1);
+pr =  find(vecTM==31.625, 1);
+plot(vecIter(pr:end),vecTM(pr:end))
 xlabel('iteration')
-ylabel('Temperaturas')
-legend('CH4','CH9','PWM');
+ylabel('Temperature [ºC]')
 
+subplot(2,1,2);
+plot(vecIter(pr:end),vecPWM(pr:end))
+xlabel('iteration')
+ylabel('Duty Cycle PWM [%]')
 
-%% Normalizacao dos dados
+%% Relação ente dados colhidos em regime permanente pv(mv)
+
+%mv PWM [duty cycle %]:       [ 9       11      13     15     17]
+%pv temperatura media [ºC]:   [ 31.668  34.10   35.80  38.50  39.93]
+plot([9 11 13 15 17],[ 31.668  34.10   35.80  38.50  39.93])
+xlabel('PWM')
+ylabel('Temperature')
+
+%% pesquisando os intervalos
 
 degraus = [];
 init = [];
 
-
-%o vetor com valores diferentes dos degraus de PWM
+%constroi vetor com valores diferentes dos degraus de PWM
 for i = 1: length(vecPWM)-1
-    
     i = i + 1;
-    
     if vecPWM(i) ~= vecPWM(i-1) & vecPWM(i) > 0
-        
-        degraus(end+1) = vecPWM(i);
-                            
+        degraus(end+1) = vecPWM(i);                            
     end 
-    
 end
-
 
 %registrando a primeira ocorrência de cada um deles dentro do vecPWM
-for i = 1 : length(degraus)
-    
+for i = 1 : length(degraus)   
    init(end+1) = find(vecPWM==degraus(i), 1);
-
 end
 
 
-%% 
-%rodar arquivo modelo MTG
+%% Normalizacao dos dados
 
-%Normalizando os valores de entrada e saída
+%u = 11 --> 13
+u = vecPWM(276367:324753); %inicio e fim do intervalo
+uN = u - 11;
 
-%Basicamente, é necessário deslocar no tempo e na amplitude todos valores
-%medidos da entrada e da saída para que o ponto de operacao inicie em zero.
-%Para isso, iniciaremos do valor 2.5V --> que agora será deslocado para 0, então 
-%toda a faixa de dados de u que é igual do primeiro valor de 2.5V ate o ultimo valor de 3V 
-%(no caso as linhas 4005:5005  da 2a coluna do array) será o ponto inicial, o mesmo é feito 
-%com a saída, com a diferença  que o valor descontado será igual ao ultimo valor 
-%onde y = 2.5V (pois ja passou o transitorio).
+%y = 34.1820 --> 35.95 
+y = vecTM(276367:324753);  %inicio e fim do intervalo
+yN = y - 34.1820;
 
-%Scopedata2 é o array gerado pelo scope do Simlulink que guarda na 
-%1a coluna: o tempo
-%2a coluna: os valores de entrada
-%3a coluna: os valores da saída
-
-% u = ScopeData2(3006:5005,2);
-% uN = u - 2.5;
-% 
-% y = ScopeData2(3006:5005,3);
-% yN = y - 2.5903;
+%configurando janela
+screenSize = get(0,'screensize'); % gets screen size
+monWidth = screenSize(3);
+monHeight = screenSize(4);
+offHeight = 0; % assumed height of system task bar
+monHeight = monHeight - offHeight; % usable screen height
+% establishing a 2x3 grid on the screen
+figHeight = monHeight/2;
+figWidth = monWidth/3;
 
 %nao normalizado (começa do ponto de operacao 2.5V)
-% figure
-% u = u(600:end) %para começar mais tarde e deixar menos pontos no '2.5'
-% y = y(600:end)
-% plot(u)
-% hold on
-% plot(y)
-% axis([0 1150 2.45 3.5])
-% xlabel('tempo de simulacao [s]')
-% ylabel('tensão [V]')
-% legend('Entrada de Controle', 'Saida Medida')
+figure
+set(gcf,'OuterPosition',[1 offHeight figWidth figHeight]);
+set(gcf,'name','Dados não normalizados')
+subplot(2,1,1)
+plot(y)
+hold on
+xlabel('iteration')
+ylabel('Temperatura [ºC]')
+hold on
+subplot(2,1,2)
+plot(u)
+xlabel('iteration')
+ylabel('duty cycle [%]')
+
+% normalizado --> ponto de operação deslocado para a origem
+figure
+set(gcf,'OuterPosition',[1 offHeight figWidth figHeight]);
+set(gcf,'name','Dados normalizados')
+subplot(2,1,1)
+plot(yN)
+xlabel('iteration')
+ylabel('Temperatura [ºC]')
+hold on
+subplot(2,1,2)
+plot(uN)
+xlabel('iteration')
+ylabel('duty cycle [%]')
+
+%% Modelo estimado pelo System Identification a partir dos dados normalizados
+
+m_est = tf(0.000163, [1 0.0001979]);
+
+%Gerando graficos 
+sim('simu')
 
 
-%normalizado --> ponto de operação deslocado para a origem
-% figure
-% uN = uN(600:end)
-% yN = yN(600:end)
-% plot(uN)
-% hold on
-% plot(yN)
-% axis([0 1150 -0.1 0.9])
-% xlabel('tempo de simulacao [s]')
-% ylabel('tensão [V]')
-% legend('Entrada de Controle Normalizada', 'Saida Medida Normalizada')
+screenSize = get(0,'screensize'); % gets screen size
+monWidth = screenSize(3);
+monHeight = screenSize(4);
+offHeight = 0; % assumed height of system task bar
+monHeight = monHeight - offHeight; % usable screen height
+% establishing a 2x3 grid on the screen
+figHeight = monHeight/2;
+figWidth = monWidth/3;
+
+%Resposta em malha aberta
+figure
+set(gcf,'OuterPosition',[1 offHeight figWidth figHeight]);
+set(gcf,'name','comparacao Resposta MA')
+plot(out.dataSimu(:,2), 'ro')
+hold on
+plot(y, '-.c')
+xlabel('Tempo [s]')
+ylabel('Temperatura [ºC]')
+legend('Modelo Estimado', 'Dados medidos')
+
+
+%% Controle e resposta em malha fechada
+
+%planta estimada
+H = m_est;
+
+%1o ajuste feito pelo tuning pid do Control system designer
+pi_cd  = tf([21.65 0.001335], [1 0]);
+pid_cd = tf([26.65 0.1126 8.395e-05], [1 0.005012 0]); 
+
+% ajuste do controlador pelo PID tune 
+C1 = pidtune(H,'PI');
+C2 = pidtune(H,'PID');
+
+sys1 = feedback(H*C1,1);
+sys2 = feedback(H*C2,1);
+sys3 = feedback(H*pi_cd,1);
+sys4 = feedback(H*pid_cd,1);
+
+[u,t] = gensig("square",75000,150000);
+lsim(sys1,sys2,sys3,sys4, u,t)
+grid on
+legend("PI","PID", "PI control", "PID control")
+
+%% Resposta em malha fechada 
+
+figure
+set(gcf,'OuterPosition',[1 offHeight figWidth figHeight]);
+set(gcf,'name','Resposta MF')
+deg =  plot(out.setpoint.time, out.setpoint.data, 'g');
+hold on
+p0 = plot(out.pv_C1.time, out.pv_C1.data, 'r');
+hold on
+p1 = plot(out.pv_C2.Time, out.pv_C2.Data, 'b');
+hold on
+p2 = plot(out.pv_picd.Time, out.pv_picd.Data, '-y');
+hold on
+p3 = plot(out.pv_pidcd.Time, out.pv_pidcd.Data, '-k');
+grid on
+xlabel('Tempo [s]')
+ylabel('Temperatura [ºC]')
+%axis([0 99.99e03 25 48]);
+legend([deg, p0,p1,p2,p3],'SP', 'PV C1','PV C2', 'PV PIcd','PV PIDcd');
+
+figure
+set(gcf,'OuterPosition',[1 offHeight figWidth figHeight]);
+set(gcf,'name','Resposta MF - Sinal de controle')
+p0 = plot(out.mv_C1.time, out.mv_C1.data, 'r');
+hold on
+p1 = plot(out.mv_C2.Time, out.mv_C2.Data, 'b');
+hold on
+p2 = plot(out.mv_picd.Time, out.mv_picd.Data, '-y');
+hold on
+p3 = plot(out.mv_pidcd.Time, out.mv_pidcd.Data, '-k');
+grid on
+xlabel('Tempo [s]')
+ylabel('Temperatura [ºC]')
+%axis([0 99.99e03 25 48]);
+legend([p0,p1,p2,p3],'MV C1','MV C2', 'MV PIcd','MV PIDcd');
 
 
 
-
-    
 
 
 
